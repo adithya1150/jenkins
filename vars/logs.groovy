a89ff7a2e43b9f2cfbd0ea05cb9df0f62aa1364c
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-24 17:57:00 +0530

    Update logs.groovy

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -1,9 +1,9 @@
-def call(){           
+def call(file){           
    script {
      sh '''
      #!/bin/sh
      set +x
-     /usr/bin/tail -f ~/erp/log/erp.log & PID=$!
+     /usr/bin/tail -f ~/$file/log/erp.log & PID=$!
      /bin/sleep 100
      /bin/kill $PID
      '''
