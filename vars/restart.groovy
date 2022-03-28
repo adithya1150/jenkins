d52fbdc4b2ee1e8dc24bca98286b245e3a9955a8
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-28 18:08:06 +0530

    Update restart.groovy

--- vars/restart.groovy
+++ vars/restart.groovy
@@ -3,7 +3,7 @@ def call (erppath)  {
        sh '''
        #!/bin/sh
        sh ~/$erppath/pidscript.sh
-       /usr/local/bin/daemonize  /home/sdwot/erp/bin/python /home/sdwot/$erppath/$folder/sdwot.py -c /home/sdwot/erp/.erp_serverrc
+       /usr/local/bin/daemonize  /home/sdwot/erp/bin/python /home/sdwot/$erppath/$folder/sdwot.py -c /home/sdwot/$erppath/.erp_serverrc
        '''
     }
 }
