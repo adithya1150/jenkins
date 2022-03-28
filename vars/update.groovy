7b0c983c59b88108b9000ce75d531eb877e6cb9e
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-28 18:22:59 +0530

    Create update.groovy

---
+++ vars/update.groovy
@@ -0,0 +1,14 @@
+def call(dbname){
+    script {
+        sh '''
+        #!/bin/sh
+        set +x
+        sh ~/$erppath/pidscript.sh
+       /usr/local/bin/daemonize  /home/sdwot/erp/bin/python /home/sdwot/$erppath/$folder/sdwot.py -c /home/sdwot/$erppath/.erp_serverrc -u all -d $dbname
+        echo "An update has been started"
+        echo "Please wait for 15 mins"
+        /bin/sleep 100
+        '''
+    }
+    restart()
+}
