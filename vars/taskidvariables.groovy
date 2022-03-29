62b7ac9fc381743a54a6a23add8bd31c69fe405f
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-29 17:24:25 +0530

    Update taskidvariables.groovy

--- vars/taskidvariables.groovy
+++ vars/taskidvariables.groovy
@@ -9,7 +9,12 @@ def call() {
                 env.prod_dbpasswd   =   "jaya"
                 env.prod_hostname   =   "jaya"
                 env.training_host   =   "192.168.181.252"                    
-                env.prod_host       =   "192.168.181.222"                    
+                env.prod_host       =   "192.168.181.222"  
+                env.apache_path     =   "/etc/httpd/conf.d"                    
+                env.ip              =   "192.168.181.248"
+                env.apache_host     =   "192.168.181.253"
+                env.public_ip       =   "14.97.205.78"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"
             break
             case 'saideepat':
                 env.prod_db         =   "saideepa" 
@@ -18,7 +23,12 @@ def call() {
                 env.prod_dbpasswd   =   "saideepa"
                 env.prod_hostname   =   "saideepa"                      
                 env.training_host   =   "192.168.181.252"                    
-                env.prod_host       =   "192.168.181.222" 
+                env.prod_host       =   "192.168.181.222"                
+                env.apache_path     =   "/etc/httpd/conf.d"
+                env.ip              =   "192.168.181.249"                    
+                env.apache_host     =   "192.168.181.253"                    
+                env.public_ip       =   "14.97.205.78"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"
             break
             case 'jayat':
                 env.prod_db         =   "jaya"
@@ -28,6 +38,11 @@ def call() {
                 env.prod_hostname   =   "jaya"              
                 env.training_host   =   "192.168.181.252"                    
                 env.prod_host       =   "192.168.181.222"
+                env.apache_path     =   "/etc/httpd/conf.d"
+                env.ip              =   "192.168.181.248"                    
+                env.apache_host     =   "192.168.181.253"                    
+                env.public_ip       =   "14.97.205.78"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"
             break
             case 'drilltecht':
                 env.prod_db         =   "drilltech"
@@ -37,6 +52,11 @@ def call() {
                 env.jailname        =   "drilltech"                 
                 env.training_host   =   "192.168.181.252"                    
                 env.prod_host       =   "192.168.181.222"
+                env.apache_path     =   "/etc/httpd/conf.d"
+                env.ip              =   "192.168.181.247"                   
+                env.apache_host     =   "192.168.181.253"                    
+                env.public_ip       =   "14.97.205.78"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"           
             break
             case 'rockbitt':
                 env.prod_db         =   "rockbit" 
@@ -45,7 +65,12 @@ def call() {
                 env.prod_dbpasswd   =   "rockbit"  
                 env.jailname        =   "rockbit"                                 
                 env.ip              =   "192.168.181.246"
-                env.training_host   =   "192.168.181.252"                                                          
+                env.training_host   =   "192.168.181.252"
+                env.apache_path     =   "/etc/httpd/conf.d"
+                env.ip              =   "192.168.181.246"                   
+                env.apache_host     =   "192.168.181.253"                    
+                env.public_ip       =   "14.97.205.78"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"
             break
             case 'rocktect':
                 env.prod_db         =   "rocktec" 
@@ -53,7 +78,12 @@ def call() {
                 env.prod_dbuser     =   "rocktec"
                 env.prod_dbpasswd   =   "rocktec"                                 
                 env.training_host   =   "192.168.6.3"                    
-                env.prod_host       =   "192.168.6.111"                                      
+                env.prod_host       =   "192.168.6.111"
+                env.apache_path     =   "/usr/local/etc/apache24/extra"
+                env.ip              =   "192.168.6.254"
+                env.apache_host     =   "192.168.6.111"
+                env.public_ip       =   "14.97.148.62"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"
             break
             case 'cenergt':
                 env.prod_db         =   "c2_prod" 
@@ -62,7 +92,12 @@ def call() {
                 env.prod_dbpasswd   =   "cenerg"
                 env.jailname        =   "cenerg"                                  
                 env.training_host   =   "192.168.6.3"                    
-                env.prod_host       =   "192.168.6.111"                                      
+                env.prod_host       =   "192.168.6.111"
+                env.apache_path     =   "/usr/local/etc/apache24/extra"                    
+                env.ip              =   "192.168.6.245"
+                env.apache_host     =   "192.168.6.111"
+                env.public_ip       =   "14.97.148.62"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/cenerg.git"           
             break
             case 'sdforgingt':
                 env.prod_db         =   "sdforging" 
@@ -71,8 +106,15 @@ def call() {
                 env.prod_dbpasswd   =   "sdf"
                 env.jailname        =   "sdf"                                   
                 env.training_host   =   "192.168.6.3"                    
-                env.prod_host       =   "192.168.6.111"                                      
+                env.prod_host       =   "192.168.6.111" 
+                env.apache_path     =   "/usr/local/etc/apache24/extra"                    
+                env.ip              =   "192.168.6.239"                
+                env.apache_host     =   "192.168.6.111"
+                env.public_ip       =   "14.97.148.62"
+                env.url             =   "ssh://git@phabricator.sdwot.ai:2223/source/saideepa.git"  
             break            
         }
     }
 }
+
+
