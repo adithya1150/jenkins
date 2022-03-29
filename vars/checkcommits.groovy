7aa995702fd9f2dac3c7d02bff258db1c7685902
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-29 11:14:38 +0530

    Update checkcommits.groovy

--- vars/checkcommits.groovy
+++ vars/checkcommits.groovy
@@ -1,7 +1,7 @@
 def call() {
          script {
              sh '''
-             cd ~/$erppath/sdwot
+             cd ~/$erppath/$folder
              git fetch
              C=`git rev-list HEAD...origin/feature/Saideepa --count`
              echo -e "$C" > ~/$erppath/commitcount
