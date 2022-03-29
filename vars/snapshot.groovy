ce2fa37bdd1609342b8207be9223b235d1f18a66
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-29 12:19:38 +0530

    Update snapshot.groovy

--- vars/snapshot.groovy
+++ vars/snapshot.groovy
@@ -6,7 +6,7 @@ def call() {
         PGPASSWORD=$prod_dbpasswd pg_dump -h $prod_host --no-owner --no-acl -U $prod_dbuser $prod_db >~/dbs/$prod_db.sql
         TIMESTAMP=`date +%d%b%y%H%M | tr '[:upper:]' '[:lower:]'`
         DBNAME=$prod_db$TIMESTAMP
-//         PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database $prod_db"
+//         PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database $prod_db;"
 //         PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser $prod_db < ~/dbs/$prod_db.sql
 //         sh ~/pidscript.sh
 //         PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "alter database $prod_db rename to $DBNAME"
