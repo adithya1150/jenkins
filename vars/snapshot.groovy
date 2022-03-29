2103181f08cb08566bf9578a62ce5abe2628a9d8
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-29 12:11:49 +0530

    Create snapshot.groovy

---
+++ vars/snapshot.groovy
@@ -0,0 +1,18 @@
+def snapshot () {
+    script {
+        sh '''
+        #!/bin/sh
+        rm -rf ~/dbs/*
+        PGPASSWORD=$prod_dbpasswd pg_dump -h $pod_host --no-owner --no-acl -U $prod_dbuser $prod_db >~/dbs/$prod_db.sql
+        TIMESTAMP=`date +%d%b%y%H%M | tr '[:upper:]' '[:lower:]'`
+        DBNAME=$prod_db$TIMESTAMP
+        PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database $prod_db"
+        PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser $prod_db < ~/dbs/$prod_db.sql
+        sh ~/pidscript.sh
+        PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "alter database $prod_db rename to $DBNAME"
+        scp -r sdwot@$prod_host:/usr/jail/$jailname/home/sdwot/erp/data/filestore/$prod_db ~/erp/data/filestore/
+        mv  ~/erp/data/filestore/$prod_db ~/erp/data/filestore/$DBNAME
+        '''
+        update($DBNAME)
+    }
+}
