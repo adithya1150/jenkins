e7855ef09870b064221638b4420fa194966a5b6c
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-30 11:10:32 +0530

    Update snapshot.groovy

--- vars/snapshot.groovy
+++ vars/snapshot.groovy
@@ -1,19 +1,18 @@
 def call() {
     script {
         sh '''
-        #!/bin/sh
-        rm -rf ~/dbs/*
-        #PGPASSWORD=$prod_dbpasswd pg_dump -h $prod_host --no-owner --no-acl -U $prod_dbuser $prod_db >~/dbs/$prod_db.sql
-        TIMESTAMP=`date +%d%b%y%H%M`
-        DBNAME="$prod_db$TIMESTAMP"
-        PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database ""$DBNAME"";"
-        #PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser $prod_db < ~/dbs/$prod_db.sql
-        #sh ~/pidscript.sh
-        #PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "alter database $prod_db rename to $DBNAME"
-        #scp -r sdwot@$prod_host:/usr/jail/$jailname/home/sdwot/erp/data/filestore/$prod_db ~/erp/data/filestore/
-        #mv  ~/erp/data/filestore/$prod_db ~/erp/data/filestore/$DBNAME
+          #!/bin/sh
+          rm -rf ~/dbs/*
+          PGPASSWORD=$prod_dbpasswd pg_dump -h $prod_host --no-owner --no-acl -U $prod_dbuser $prod_db >~/dbs/$prod_db.sql
+          TIMESTAMP=`date +%d%b%y%H%M`
+          DBNAME='"'$prod_db$TIMESTAMP'"'
+          PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database $DBNAME;"        
+          PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser $prod_db < ~/dbs/$prod_db.sql
+          sh ~/pidscript.sh
+          PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "alter database $prod_db rename to $DBNAME"
+          scp -r sdwot@$prod_host:/usr/jail/$jailname/home/sdwot/erp/data/filestore/$prod_db ~/erp/data/filestore/
+          mv  ~/erp/data/filestore/$prod_db ~/erp/data/filestore/$DBNAME
         '''
-        logs()
-        //update($DBNAME)
+        update($DBNAME)
     }
 }
