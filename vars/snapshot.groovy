545e7b72010d2f93e0b8a22f395cea5deffba258
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-03-30 10:21:23 +0530

    Update snapshot.groovy

--- vars/snapshot.groovy
+++ vars/snapshot.groovy
@@ -1,13 +1,12 @@
 def call() {
     script {
-        sh 'TIMESTAMP=`date +%d%b%y%H%M | tr '[:upper:]' '[:lower:]'`'
         sh '''
         #!/bin/sh
         rm -rf ~/dbs/*
-        PGPASSWORD=$prod_dbpasswd pg_dump -h $prod_host --no-owner --no-acl -U $prod_dbuser $prod_db >~/dbs/$prod_db.sql
-        TIMESTAMP=`date +%d%b%y%H%M | tr '[:upper:]' '[:lower:]'`
+        #PGPASSWORD=$prod_dbpasswd pg_dump -h $prod_host --no-owner --no-acl -U $prod_dbuser $prod_db >~/dbs/$prod_db.sql
+        TIMESTAMP=`date +%d%b%y%H%M`
         DBNAME=$prod_db$TIMESTAMP
-        #PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database $prod_db;"
+        PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "create database "$DBNAME";"
         #PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser $prod_db < ~/dbs/$prod_db.sql
         #sh ~/pidscript.sh
         #PGPASSWORD=$prod_dbpasswd psql -h $training_host -U $prod_dbuser postgres -c "alter database $prod_db rename to $DBNAME"
@@ -15,7 +14,6 @@ def call() {
         #mv  ~/erp/data/filestore/$prod_db ~/erp/data/filestore/$DBNAME
         logs()
         '''
-        echo $count
         //update($DBNAME)
     }
 }
