d4be5687bb00cb004a5c03392c89457a643b6030
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-06-27 21:07:27 +0530

    Update api.groovy

--- vars/api.groovy
+++ vars/api.groovy
@@ -1,12 +1,9 @@
 def call(){
-    script {
         sh '''
-         #!/bin/bash
          pwd
          sh ~/.stopservices.sh
          rm -rf node_modules
          npm update -f
          nohup  npm start &
          '''
-    }
 }
