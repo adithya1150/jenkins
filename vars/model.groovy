e3030e45068ae9a9170896ba730e7eb021c94c03
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-06-28 11:15:52 +0530

    Create model.groovy

---
+++ vars/model.groovy
@@ -0,0 +1,7 @@
+def call (){
+        sh '''
+        #!/bin/bash
+        cd $backend_path/model
+        ~/model-venv/bin/python3 -m pip install -r $backend_path/model/requirements.txt        
+        nohup ~/model-venv/bin/python3 app.py &
+}
