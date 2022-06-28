000e14886fd827c077cc884f012c514691dd1d7d
adithya1150 <78546556+adithya1150@users.noreply.github.com>
2022-06-28 11:13:38 +0530

    Create store.groovy

---
+++ vars/store.groovy
@@ -0,0 +1,6 @@
+def call(){
+      sh '''
+      #!/bin/bash
+      ~/store-venv/bin/python3 -m pip install -r $backend_path/store/requirements.txt
+      nohup ~/store-venv/bin/python3 $backend_path/store/app.py &
+}
